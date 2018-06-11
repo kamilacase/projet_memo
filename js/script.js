d8eb2af77f7e79bd1b599e5c5487192d49374476
kamilacase <xakdxshooter@live.fr>
2018-06-11 11:57:23 +0200

    fin js

--- js/script.js
+++ js/script.js
@@ -1,179 +1,65 @@
-(function(){
+var motifsCartes=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+var etatsCartes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var cartesRetournees=[];
+var nbPairesTrouvees=0;
+var imgCartes=document.getElementById("tapis").getElementsByTagName("img");
 
-	var Memory = {
-
-		init: function(cards){
-			this.$game = $(".game");
-			this.$modal = $(".modal");
-			this.$overlay = $(".modal-overlay");
-			this.$restartButton = $("button.restart");
-			this.cardsArray = $.merge(cards, cards);
-			this.shuffleCards(this.cardsArray);
-			this.setup();
-		},
-
-		shuffleCards: function(cardsArray){
-			this.$cards = $(this.shuffle(this.cardsArray));
-		},
-
-		setup: function(){
-			this.html = this.buildHTML();
-			this.$game.html(this.html);
-			this.$memoryCards = $(".card");
-			this.paused = false;
-     	this.guess = null;
-			this.binding();
-		},
-
-		binding: function(){
-			this.$memoryCards.on("click", this.cardClicked);
-			this.$restartButton.on("click", $.proxy(this.reset, this));
-		},
-		// kinda messy but hey
-		cardClicked: function(){
-			var _ = Memory;
-			var $card = $(this);
-			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
-				$card.find(".inside").addClass("picked");
-				if(!_.guess){
-					_.guess = $(this).attr("data-id");
-				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
-					$(".picked").addClass("matched");
-					_.guess = null;
-				} else {
-					_.guess = null;
-					_.paused = true;
-					setTimeout(function(){
-						$(".picked").removeClass("picked");
-						Memory.paused = false;
-					}, 600);
-				}
-				if($(".matched").length == $(".card").length){
-					_.win();
-				}
-			}
-		},
-
-		win: function(){
-			this.paused = true;
-			setTimeout(function(){
-				Memory.showModal();
-				Memory.$game.fadeOut();
-			}, 1000);
-		},
-
-		showModal: function(){
-			this.$overlay.show();
-			this.$modal.fadeIn("slow");
-		},
-
-		hideModal: function(){
-			this.$overlay.hide();
-			this.$modal.hide();
-		},
-
-		reset: function(){
-			this.hideModal();
-			this.shuffleCards(this.cardsArray);
-			this.setup();
-			this.$game.show("slow");
-		},
-
-		// Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
-		shuffle: function(array){
-			var counter = array.length, temp, index;
-	   	// While there are elements in the array
-	   	while (counter > 0) {
-        	// Pick a random index
-        	index = Math.floor(Math.random() * counter);
-        	// Decrease counter by 1
-        	counter--;
-        	// And swap the last element with it
-        	temp = array[counter];
-        	array[counter] = array[index];
-        	array[index] = temp;
-	    	}
-	    	return array;
-		},
-
-		buildHTML: function(){
-			var frag = '';
-			this.$cards.each(function(k, v){
-				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
-				<div class="front"><img src="'+ v.img +'"\
-				alt="'+ v.name +'" /></div>\
-				<div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
-				alt="Codepen" /></div></div>\
-				</div>';
-			});
-			return frag;
+for(var i=0;i<imgCartes.length;i++){
+	imgCartes[i].noCarte=i; //Ajout de la propriété noCarte à l'objet img
+	imgCartes[i].onclick=function(){
+		controleJeu(this.noCarte);
+	}
+}
+initialiseJeu();
+function majAffichage(noCarte){
+	switch(etatsCartes[noCarte]){
+		case 0:
+			imgCartes[noCarte].src="../css/img/back.png";
+			break;
+		case 1:
+			imgCartes[noCarte].src="../css/img/carte"+motifsCartes[noCarte]+".png";
+			break;
+		case -1:
+			imgCartes[noCarte].style.visibility="hidden";
+			break;
+	}
+}
+function rejouer(){
+	alert("Bravo !");
+	location.reload();
+}
+function initialiseJeu(){
+	for(var position=motifsCartes.length-1; position>=1; position--){
+		var hasard=Math.floor(Math.random()*(position+1));
+		var sauve=motifsCartes[position];
+		motifsCartes[position]=motifsCartes[hasard];
+		motifsCartes[hasard]=sauve;
+	}
+}
+function controleJeu(noCarte){
+    if(cartesRetournees.length<2){
+        if(etatsCartes[noCarte]==0){
+			etatsCartes[noCarte]=1;
+			cartesRetournees.push(noCarte);
+			majAffichage(noCarte);
 		}
-	};
+        if(cartesRetournees.length==2){
+        			var nouveauEtat=0;
+        			if(motifsCartes[cartesRetournees[0]]==motifsCartes[cartesRetournees[1]]){
+        				nouveauEtat=-1;
+        				nbPairesTrouvees++;
+        			}
 
-	var cards = [
-		{
-			name: "php",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
-			id: 1,
-		},
-		{
-			name: "css3",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
-			id: 2
-		},
-		{
-			name: "html5",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
-			id: 3
-		},
-		{
-			name: "jquery",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
-			id: 4
-		},
-		{
-			name: "javascript",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
-			id: 5
-		},
-		{
-			name: "node",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
-			id: 6
-		},
-		{
-			name: "photoshop",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
-			id: 7
-		},
-		{
-			name: "python",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
-			id: 8
-		},
-		{
-			name: "rails",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
-			id: 9
-		},
-		{
-			name: "sass",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
-			id: 10
-		},
-		{
-			name: "sublime",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
-			id: 11
-		},
-		{
-			name: "wordpress",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
-			id: 12
-		},
-	];
-
-	Memory.init(cards);
-
-
-})();
+        			etatsCartes[cartesRetournees[0]]=nouveauEtat;
+        			etatsCartes[cartesRetournees[1]]=nouveauEtat;
+                    setTimeout(function(){
+                    				majAffichage(cartesRetournees[0]);
+                    				majAffichage(cartesRetournees[1]);
+                    				cartesRetournees=[];
+                    				if(nbPairesTrouvees==10){
+                    					rejouer();
+                    				}
+                    			},750);
+                    		}
+                    	}
+                    }
