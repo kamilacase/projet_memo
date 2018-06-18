1180504511fc5f2b4d982bb23aabdbd1f55df7a7
kamilacase <xakdxshooter@live.fr>
2018-06-18 16:33:10 +0200

    convertion star wars

---
+++ script.js
@@ -0,0 +1,92 @@
+var motifsCartes=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+var etatsCartes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+// var fondCoquin=[1,2,3,4,5,6,7,8,9,10];
+var cartesRetournees=[];
+var nbPairesTrouvees=0;
+var imgCartes=document.getElementById("tapis").getElementsByTagName("img");
+// var imgCoquin=document.getElementById('tapis');
+
+window.onload = function randombg(){
+    var random= Math.floor(Math.random() * 10) + 0;
+    var fondCoquin = ["url('bar1.gif')",
+    "url('bar2.gif')",
+    "url('bar3.gif')",
+    "url('bar4.gif')",
+    "url('bar5.gif')",
+    "url('bar6.gif')",
+    "url('bar7.gif')",
+    "url('bar8.gif')",
+    "url('bar9.gif')",
+    "url('bar10.gif')"
+];
+document.getElementById("tapis").style.backgroundImage=fondCoquin[random];
+}
+
+for(var i=0;i<imgCartes.length;i++){
+	imgCartes[i].noCarte=i; //Ajout de la propriété noCarte à l'objet img
+	imgCartes[i].onclick=function(){
+		controleJeu(this.noCarte);
+	}
+}
+initialiseJeu();
+
+
+function majAffichage(noCarte){
+	switch(etatsCartes[noCarte]){
+		case 0:
+			imgCartes[noCarte].src="back.png";
+			break;
+		case 1:
+			imgCartes[noCarte].src="carte"+motifsCartes[noCarte]+".png";
+			break;
+		case -1:
+			imgCartes[noCarte].style.visibility="hidden";
+			break;
+	}
+
+}
+
+function rejouer(){
+	alert("Hé ben, gros cochon !");
+	location.reload();
+    // randombg();
+    url:"Shake It Baby-SoundBible.com-763292018.mp3"
+}
+
+
+function initialiseJeu(){
+	for(var position=motifsCartes.length-1; position>=1; position--){
+		var hasard=Math.floor(Math.random()*(position+1));
+		var sauve=motifsCartes[position];
+		motifsCartes[position]=motifsCartes[hasard];
+		motifsCartes[hasard]=sauve;
+	}
+}
+
+function controleJeu(noCarte){
+    if(cartesRetournees.length<2){
+        if(etatsCartes[noCarte]==0){
+			etatsCartes[noCarte]=1;
+			cartesRetournees.push(noCarte);
+			majAffichage(noCarte);
+		}
+        if(cartesRetournees.length==2){
+        			var nouveauEtat=0;
+        			if(motifsCartes[cartesRetournees[0]]==motifsCartes[cartesRetournees[1]]){
+        				nouveauEtat=-1;
+        				nbPairesTrouvees++;
+        			}
+
+        			etatsCartes[cartesRetournees[0]]=nouveauEtat;
+        			etatsCartes[cartesRetournees[1]]=nouveauEtat;
+                    setTimeout(function(){
+                    				majAffichage(cartesRetournees[0]);
+                    				majAffichage(cartesRetournees[1]);
+                    				cartesRetournees=[];
+                    				if(nbPairesTrouvees==10){
+                    					rejouer();
+                    				}
+                    			},750);
+                    		}
+                    	}
+                    }
